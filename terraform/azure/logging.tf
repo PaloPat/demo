c48e12321155663189618364b7540590cd83fda5
PaloPat <149431870+PaloPat@users.noreply.github.com>
2023-12-21 10:36:29 -0600

    Fixed code with Palo Alto!

--- terraform/azure/logging.tf
+++ terraform/azure/logging.tf
@@ -1,4 +1,5 @@
 resource azurerm_monitor_log_profile "logging_profile" {
+  # checkov:skip=1003709418909076480_AZR_1681190513864: ADD REASON
   storage_account_id = azurerm_storage_account.example.id
   categories         = ["Action"]
   locations          = [var.location]
